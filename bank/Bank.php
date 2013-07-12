c5f1eeb0a11cea3c71cf3c656fc3ab249df3115a
Omattyao <omattyao@gmail.com>
2013-07-12 16:44:01 +0900

    fixed some issues.

--- bank/Bank.php
+++ bank/Bank.php
@@ -48,44 +48,51 @@ class ExamplePlugin implements Plugin{
 									'bank' => DEFAULT_ASSETS
 							)
 					));
-					$this->api->chat->broadcast("[PocketMoney]$target has been registered.");
+					$this->api->chat->broadcast("[Bank]$target has been registered.");
 				}
 		}
 	}
 	
-	public function handleCommand($cmd, $arg){
+	public function handleCommand($cmd, $arg, $issuer, $alias){
 		$output = "";
 		switch($cmd){
 			case "bank":
+			if (!($issuer instanceof Player))
+			{
+				$output .= "Please use this command in game.\n"
+				break;
+			}
 			$subCommand = $args[0];
+			$username = $issuer->username;
 			$cfg = $this->api->plugin->readYAML($this->path . "config.yml");
 				switch($subCommand){
 					case "";
-					  $output .= "===[Bank :: Commands]===";
-					  $output .= "[Bank]/bank :: shows commands";
-					  $output .= "[Bank]/bank deposite (amount) :: put in your pocketmoney into the bank";
-					  $output .= "[Bank]/bank withdraw (amount) :: take out your pocketmoney from the bank";
-					  $output .= "[Bank]/bank loan (amount) :: get a loan from the bank";
-					  $output .= "[Bank]/bank payloan (amount) :: return your loan to the bank";
-					  $output .= "[Bank]/bank money :: shows your bank total assets";
+					  $output .= "===[Bank :: Commands]===\n";
+					  $output .= "[Bank]/bank :: shows commands\n";
+					  $output .= "[Bank]/bank deposite (amount) :: put in your pocketmoney into the bank\n";
+					  $output .= "[Bank]/bank withdraw (amount) :: take out your pocketmoney from the bank\n";
+					  $output .= "[Bank]/bank loan (amount) :: get a loan from the bank\n";
+					  $output .= "[Bank]/bank payloan (amount) :: return your loan to the bank\n";
+					  $output .= "[Bank]/bank money :: shows your bank total assets\n";
 						break;
 					case "money":
+						$target = $username;
 						if(!array_key_exists($target, $cfg))
 						{
-							$output .= "[Bank]You don't have a bank account";
+							$output .= "[Bank]You don't have a bank account\n";
 							break;
 						}
 						$money = $cfg[$issuer->username]['bank'];
-						$output .= "[Bank]You have $money in your bank account";
+						$output .= "[Bank]You have $money in your bank account\n";
 						break;
 					case "deposite":
-						$playerBank = $issuer->username;
-						$amount = $args[2];
+						$playerBank = $username;
+						$amount = $args[1];
 						$bankMoney = $cfg[$playerBank]['bank'];
 						$playerMoney = $this->api->dhandle("money.player.get", array('username' => $username));
-						if(!is_numeric($amount) or $amount < 0 or $playerMoney < $amount)
+						if(!is_numeric($amount) or $amount <= 0 or $playerMoney < $amount)
 						{
-							$output .= "[Bank]check your money / check your input number / check your spelling";
+							$output .= "[Bank]check your money / check your input number / check your spelling\n";
 							break;
 						}
 						$bankMoney += $amount;
@@ -93,7 +100,7 @@ class ExamplePlugin implements Plugin{
 						$this->api->dhandle("money.handle", array(
 										'username' => $playerBank,
 										'method' => 'grant',
-										'amount' => -$amount
+										'amount' => $amount
 								));
 						$result = array(
 								$playerBank => array(
@@ -101,16 +108,16 @@ class ExamplePlugin implements Plugin{
 								),
 								);
 						$this->overwriteConfig($result);
-						$output .= "[Bank]You have deposited to your bank acount safely";
+						$output .= "[Bank]You have deposited to your bank acount safely\n";
 						break;
 					case "withdraw":
-						$playerBank = $issuer->username;
-						$amount = $args[2];
+						$playerBank = $username;
+						$amount = $args[1];
 						$bankMoney = $cfg[$playerBank]['bank'];
 						$playerMoney = $this->api->dhandle("money.player.get", array('username' => $username));
-						if(!is_numeric($amount) or $amount < 0 or $bankMoney < $amount)
+						if(!is_numeric($amount) or $amount <= 0 or $bankMoney < $amount)
 						{
-							$output .= "[Bank]check your money / check your input number / check your spelling";
+							$output .= "[Bank]check your money / check your input number / check your spelling\n";
 							break;
 						}
 						$bankMoney -= $amount;
@@ -126,7 +133,7 @@ class ExamplePlugin implements Plugin{
 								),
 								);
 						$this->overwriteConfig($result);
-						$output .= "[Bank]You have deposited to your bank acount safely";
+						$output .= "[Bank]You have withdrown to your bank acount safely\n";
 						break;
 				}
 			break;
